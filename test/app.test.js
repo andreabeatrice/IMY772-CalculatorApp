9f2bd6bb31e3a20dd1f19917e4c2ec0d8b3e8fea
andreabeatrice <86042775+andreabeatrice@users.noreply.github.com>
2024-05-30 21:07:45 +0200

    Drafting DOM tests
    - Creating the first couple of DOM tests
    - Importing the appropriate modules needed to test them
    - Making the describe() and it() functions slightly more descriptive

--- test/app.test.js
+++ test/app.test.js
@@ -2,11 +2,16 @@ import chai from "chai";
 import chaiHttp from "chai-http";
 const should = chai.should();
 import server from "../src/app.js";
+import chaiDom from "chai-dom";
+import $ from "jquery";
+import request from "request";
 
 chai.use(chaiHttp);
+chai.use(chaiDom);
 
-describe("Server Tests", function () {
-  it("Check Server Status", function (done) {
+//Needs to start a server
+describe("The state of the server", function() {
+  it("should be running", function(done) {
     chai
       .request(server)
       .get("/")
@@ -17,3 +22,22 @@ describe("Server Tests", function () {
       });
   });
 });
+
+describe("The contents of the page", function() {
+  describe("Input 1", function() {
+
+    it("should exist", function(done) {
+      request("http://localhost:4000", function(error, response, body) {
+        body.should.have.descendants("input");
+        done();
+      });
+
+      it("should have an id", function(done) {
+        request("http://localhost:4000", function(error, response, body) {
+          $('input').should.have.id('#input1');
+          done();
+        });
+
+      });
+    });
+  });
