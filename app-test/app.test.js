51aaac8a6c05b7bc051705a4a66b55f7925a3855
andreabeatrice <86042775+andreabeatrice@users.noreply.github.com>
2024-06-02 13:36:52 +0200

    Add jest tests for input #1 and #2

--- app-test/app.test.js
+++ app-test/app.test.js
@@ -2,9 +2,53 @@
  * @jest-environment jsdom
  */
 
-import $ from "jquery";
-
 test("use jsdom in this test file", () => {
   const element = document.createElement("div");
   expect(element).not.toBeNull();
+});
+
+describe("Input #1", () => {
+  test("Page has input #1", () => {
+    return expect(
+      Promise.resolve(document.getElementById("input1")),
+    ).resolves.not.toBeNull();
+  });
+
+  test("Input #1 has a maxlength=3", () => {
+    return expect(
+      Promise.resolve(document.getElementById("input1").maxlength()),
+    ).resolves.toBe("3");
+  });
+
+  test("Input #1 is required", () => {
+    return expect(Promise.resolve(document.getElementById("input1").required()))
+      .resolves.not()
+      .toBeNull();
+  });
+});
+
+describe("Input #2", () => {
+  test("Page has input #2", () => {
+    return expect(
+      Promise.resolve(document.getElementById("input2")),
+    ).resolves.not.toBeNull();
+  });
+
+  test("Input #2 has a maxlength=3", () => {
+    return expect(
+      Promise.resolve(document.getElementById("input2").maxlength()),
+    ).resolves.toBe("3");
+  });
+
+  test("Input #2 is required", () => {
+    return expect(Promise.resolve(document.getElementById("input2").required()))
+      .resolves.not()
+      .toBeNull();
+  });
+});
+
+describe("Operations input", () => {
+  describe("Operations input", () => {
+    test("Input #2 is required", () => {});
+  });
 });
