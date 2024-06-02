09e29de26a08bd91bc0dccbae7a75a4c7710c2f1
andreabeatrice <86042775+andreabeatrice@users.noreply.github.com>
2024-06-02 15:06:11 +0200

    Update calculator.mjs

--- src/calculator.mjs
+++ src/calculator.mjs
@@ -3,15 +3,40 @@ class Calculator {
 
   constructor(hex1, hex2, op) { }
 
-  checkHexadecimal() { }
+  checkHexadecimal(hex) {
+    var re = new RegExp("^[0-9A-F]{1,3}$");
+    return re.test(hex);
 
-  convertToDecimal() { }
+  }
 
-  arithmetic() { }
+  convertToDecimal(hex) {
+    return parseInt(hex, 16);
+  }
 
-  convertToHex() { }
+  arithmetic(dec1, dec2, op) {
+    switch (op) {
+      case "add":
+        return dec1 + dec2;
+        break;
+      case "sub":
+        return dec1 - dec2;
+        break;
+      case "mul":
+        return dec1 * dec2;
+        break;
+      case "div":
+        return Math.round(dec1 / dec2);
+        break;
+      default:
+        break;
+    }
+  }
 
-  go() { }
+  convertToHex(dec) {
+    return parseInt(dec, 10).toString(16);
+  }
+
+  go(hex1, hex2, op) { }
 }
 
 export const Calculator = Calculator;
