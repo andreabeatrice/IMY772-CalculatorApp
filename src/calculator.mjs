646db4e2553e5f5658e930b301d2cd87f1e1a8b9
andreabeatrice <86042775+andreabeatrice@users.noreply.github.com>
2024-06-02 14:59:36 +0200

    Update calculator.mjs

---
+++ src/calculator.mjs
@@ -0,0 +1,17 @@
+class Calculator {
+  constructor() { }
+
+  constructor(hex1, hex2, op) { }
+
+  checkHexadecimal() { }
+
+  convertToDecimal() { }
+
+  arithmetic() { }
+
+  convertToHex() { }
+
+  go() { }
+}
+
+export const Calculator = Calculator;
