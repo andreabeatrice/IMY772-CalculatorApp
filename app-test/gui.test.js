094ac357d014849c3545c9cdfaced23db86b2a38
andreabeatrice <86042775+andreabeatrice@users.noreply.github.com>
2024-06-05 21:14:59 +0200

    GUI updated + first test

--- app-test/gui.test.js
+++ app-test/gui.test.js
@@ -6,6 +6,7 @@ import { app } from "../src/app.mjs";
 import { parse } from "node-html-parser";
 const Calculator = require("../src/calculator.mjs").Calculator;
 var server, root;
+const $ = require("jquery");
 
 test("use jsdom in this test file", async () => {
   const element = document.createElement("div");
@@ -20,4 +21,23 @@ beforeAll(async () => {
 
 afterAll(() => {
   app.close();
+});
+
+describe("Keypad", () => {
+  describe("Layout", () => {
+    test("Container DIV", async () => {
+      expect(root.querySelector(".container")).not.toBeNull();
+    });
+  });
+
+  describe("Number inputs", () => {
+    test("1 button", async () => {
+      expect(root.querySelector("#one")).not.toBeNull();
+      expect(root.querySelector("#one")._attrs.type).toBe("button");
+      expect(root.querySelector("#one").childNodes[0]._rawText).toBe("1");
+      expect(root.querySelector("#one")._attrs.onclick).toBe(
+        "document.getElementById('input1').setAttribute('value','1');",
+      );
+    });
+  });
 });
